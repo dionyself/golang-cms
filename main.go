9e8719cdf0cfc7fe2b9d208b9917797d6831935e
Dionys Rosario <dionyself@gmail.com>
2016-04-28 08:40:43 -0400

    partial cache

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 )
 
 func init() {
+	//beego.BConfig.WebConfig.Session.SessionProviderConfig = `127.0.0.2:6379,100`
 	DbAddress := ""
 	DbEngine := beego.AppConfig.String("DB_Engine")
 	DbServerPort := beego.AppConfig.String("DB_ServerPort")
