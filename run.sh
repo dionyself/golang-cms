8346737ad0e5ee7caf7a1dbe4eca3bb92c2d1c9e
Dionys Rosario <dionyself@gmail.com>
2023-01-05 18:53:34 -0400

    udt

--- run.sh
+++ run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
 cd /app/golang-cms
-git pull origin master --tags
-export GOLANG_CMS_VERSION=$(git tag --sort=committerdate | tail -1)
-git checkout $GOLANG_CMS_VERSION
-echo "Current version is $GOLANG_CMS_VERSION"
-go get -u github.com/beego/bee/v2
-go mod tidy
+echo "Current version is $(git tag --sort=committerdate | tail -1)"
 bee run
